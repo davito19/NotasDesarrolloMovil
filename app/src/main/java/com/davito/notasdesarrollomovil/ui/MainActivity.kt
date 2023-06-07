5d0e39c4b18092d9c292a557f20a7322aa4fa493
David Bonett <omardbonett@gmail.com>
2023-06-07 17:57:56 -0500

    version1.0

--- app/src/main/java/com/davito/notasdesarrollomovil/ui/MainActivity.kt
+++ app/src/main/java/com/davito/notasdesarrollomovil/ui/MainActivity.kt
@@ -1,7 +1,8 @@
 package com.davito.notasdesarrollomovil.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.davito.notasdesarrollomovil.databinding.ActivityMainBinding
 import java.text.DecimalFormat
 
@@ -18,15 +19,22 @@ class MainActivity : AppCompatActivity() {
         val df = DecimalFormat("#.##")
 
         binding.calcularButton.setOnClickListener {
-            if (!(binding.nota60EditText.text.isNullOrEmpty() || binding.nota7EditText.text.isNullOrEmpty() || binding.nota8EditText.text.isNullOrEmpty() || binding.notaFinalEditText.text.isNullOrEmpty())){
+            if (!(binding.nota60EditText.text.isNullOrEmpty() || binding.nota7EditText.text.isNullOrEmpty() || binding.nota8EditText.text.isNullOrEmpty() || binding.notaFinalEditText.text.isNullOrEmpty())) {
                 val nota60 = binding.nota60EditText.text.toString().toDouble()
                 val nota7 = binding.nota7EditText.text.toString().toDouble()
                 val nota8 = binding.nota8EditText.text.toString().toDouble()
                 val notaFinal = binding.notaFinalEditText.text.toString().toDouble()
-                val resultado = df.format((nota60*0.60)+(nota7*0.07)+(nota8*0.08)+(notaFinal*0.25))
-                binding.textView2.text=resultado.toString()
+                val resultado =
+                    df.format((nota60 * 0.60) + (nota7 * 0.07) + (nota8 * 0.08) + (notaFinal * 0.25))
+                binding.textView2.text = resultado.toString()
             } else {
-                binding.textView2.text=""
+                binding.textView2.text = ""
+
+                Toast.makeText(
+                    applicationContext,
+                    "todos los campos deben ser diligenciados",
+                    Toast.LENGTH_LONG
+                ).show()
             }
 
         }
